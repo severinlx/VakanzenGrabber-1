74a4a11af4479c597f131d6aaf2b457d02cca8c2
severinlx <severinlx@yahoo.com>
2021-02-09 16:54:25 +0100

    Update Test.java
    Comment out the test

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -5,6 +5,6 @@ public class Test {
     @org.testng.annotations.Test // Ausführbarkeit von der Funktion als Test
     public void Test() throws InterruptedException {
         // Da wir keine Argumente (bisher) erwarten, wird ein leeres String Array übereben
-        Main.main(new String[1]);
+        //Main.main(new String[1]);
     }
 }
